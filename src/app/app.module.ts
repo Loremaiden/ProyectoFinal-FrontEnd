6245821d51913e8b2a82528f2898a9f805707b63
Loremaiden <loremaiden@gmail.com>
2023-03-05 16:02:25 -0300

    cargan las imagenes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { LogoapComponent } from './componentes/logoap/logoap.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { SocialComponent } from './componentes/social/social.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoapComponent,
+    BannerComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule,
