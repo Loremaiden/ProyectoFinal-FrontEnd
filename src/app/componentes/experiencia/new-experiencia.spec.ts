a0d5fd2a59699fcccc1d8c7c3e15da2755699abf
Loremaiden <loremaiden@gmail.com>
2023-05-24 23:17:00 -0300

    carpeta extra el

--- src/app/componentes/experiencia/new-experiencia.spec.ts
+++
@@ -1,7 +0,0 @@
-import { NewExperiencia } from './new-experiencia';
-
-describe('NewExperiencia', () => {
-  it('should create an instance', () => {
-    expect(new NewExperiencia()).toBeTruthy();
-  });
-});
